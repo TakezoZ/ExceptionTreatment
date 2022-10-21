bd597984e4b4239d5e1d2ad4c9a62c3da4300b19
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-20 21:02:03 -0300

    Estrutura antes do tratamento de Exeção

---
+++ EstruturaTry_Catch/EstruturaTry_Catch/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EstruturaTry_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            int result = n1 / n2;
+            Console.WriteLine(result);
+        }
+
+    }
+}
